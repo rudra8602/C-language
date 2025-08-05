1d86f015f89c422e7d9db12d29bf8f3b972d0ee2
rudra8602 <162789390+rudra8602@users.noreply.github.com>
2025-08-05 18:42:18 +0530

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a=50,b=70,c=10;
+    if(a>b){
+        printf("a is greater");
+    }
+    else if (b>c)
+    {
+        printf("b is greater");
+    }
+    else{
+        printf("c is greater");
+    }
+    return 0;
+}
